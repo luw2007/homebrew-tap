05a3f0a15a78b78de6a7db2c9ceba460098dd5f7
luw2007 <Luw2007@gmail.com>
2026-03-11 06:46:34 +0800

    Update jf to v0.1.4

--- Formula/jf.rb
+++ Formula/jf.rb
@@ -2,25 +2,25 @@ class Jf < Formula
   desc "LLM-optimized JSON formatter for better readability and token efficiency"
   homepage "https://github.com/luw2007/llm_json_formatter"
   license "MIT"
-  version "0.1.3"
+  version "0.1.4"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/luw2007/llm_json_formatter/releases/download/v#{version}/jf-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "45550c9d489dcacfb0ec919f98c8f72b01594f7d9951691648b662589b2e5196"
+      sha256 "55076cf48fd62c21a696254dade81d90747ae1404115691a44668b1dbe815c4e"
     else
       url "https://github.com/luw2007/llm_json_formatter/releases/download/v#{version}/jf-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "5f58046e2043e30fc0092b51c7c228f3d244ada73b2ed1c3fa429c78f39cd58f"
+      sha256 "a8e437cd5f5f5a068923af7cded5b70cc57eb8d2a2274715ad2034a257610224"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/luw2007/llm_json_formatter/releases/download/v#{version}/jf-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "ba98210c302edc6cd6b4832f94fcd5c6095e3a90e755864d173e7a2398e42314"
+      sha256 "a001088802beeaaf8ae29c432d653c0a11b76d34598f0df8b15b1cbd3af4210d"
     else
       url "https://github.com/luw2007/llm_json_formatter/releases/download/v#{version}/jf-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "530480cca864c7a62d56d3a009fb45a72d2e2f893bd919c9e2b0d1cb97d99fee"
+      sha256 "a060812f4c668fdb4bfaa893efbcf3e8336a25b804f604fface75a5356347b50"
     end
   end
 
