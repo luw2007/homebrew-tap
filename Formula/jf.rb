b247244375d2759e8c985e89889c853f60848485
luw2007 <luw2007@gmail.com>
2026-03-13 15:49:31 +0800

    fix: correct jf v0.1.6 sha256 values

--- Formula/jf.rb
+++ Formula/jf.rb
@@ -7,24 +7,20 @@ class Jf < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/luw2007/llm_json_formatter/releases/download/v0.1.6/jf-v0.1.6-aarch64-apple-darwin.tar.gz"
-      sha256 "Downloading https://github.com/luw2007/llm_json_formatter/releases/download/v0.1.6/jf-v0.1.6-aarch64-apple-darwin.tar.gz ...
-f1d0b192552ae1ba7ae2d3363c9ea990abc77bd124ea5662c70a75c15c91029b"
+      sha256 "f1d0b192552ae1ba7ae2d3363c9ea990abc77bd124ea5662c70a75c15c91029b"
     else
       url "https://github.com/luw2007/llm_json_formatter/releases/download/v0.1.6/jf-v0.1.6-x86_64-apple-darwin.tar.gz"
-      sha256 "Downloading https://github.com/luw2007/llm_json_formatter/releases/download/v0.1.6/jf-v0.1.6-x86_64-apple-darwin.tar.gz ...
-091dec5da7bf3dafd456b9c69eea0fd3386009555f03bc73777ca8a6418351dc"
+      sha256 "091dec5da7bf3dafd456b9c69eea0fd3386009555f03bc73777ca8a6418351dc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/luw2007/llm_json_formatter/releases/download/v0.1.6/jf-v0.1.6-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "Downloading https://github.com/luw2007/llm_json_formatter/releases/download/v0.1.6/jf-v0.1.6-aarch64-unknown-linux-gnu.tar.gz ...
-fb144984de8466268163e27461bb5fdd68f8dc2db31ad78af760468d8b0eff1a"
+      sha256 "fb144984de8466268163e27461bb5fdd68f8dc2db31ad78af760468d8b0eff1a"
     else
       url "https://github.com/luw2007/llm_json_formatter/releases/download/v0.1.6/jf-v0.1.6-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "Downloading https://github.com/luw2007/llm_json_formatter/releases/download/v0.1.6/jf-v0.1.6-x86_64-unknown-linux-gnu.tar.gz ...
-1a90a661a9c094d9efd4d5c05b9fa6a4d371a68a69cbdb359ea28f505a974bfe"
+      sha256 "1a90a661a9c094d9efd4d5c05b9fa6a4d371a68a69cbdb359ea28f505a974bfe"
     end
   end
 
